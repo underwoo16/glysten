da37364ff18500dbe9e25afc9d83480f860e525e
Chandler Underwood <underwoo16@up.edu>
2019-07-26 08:51:57 -0700

    Make artist squares clickable and have pointer cursor on focus

--- src/components/artistView/ArtistSquare.tsx
+++ src/components/artistView/ArtistSquare.tsx
@@ -3,11 +3,17 @@ import './ArtistView.css';
 import '../../css/skeleton.css'
 
 export class ArtistSquare extends React.Component<SpotifyApi.ArtistObjectFull> {
+
+  openArtistLink() {
+    let artist = this.props
+    window.open(artist.external_urls.spotify, "_blank");
+  }
+
   render() {
     let artist = this.props;
 
     return (
-      <div className='artist-square one-third column'>
+      <div className='artist-square one-third column' onClick={() => this.openArtistLink()}>
         <h3 className='content'> {artist.name} </h3>
         <figure className='artist-image u-max-full-width'>
           <img src={artist.images[0].url} alt={artist.name}></img>
